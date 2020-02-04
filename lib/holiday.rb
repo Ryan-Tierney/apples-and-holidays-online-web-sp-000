7bb2a92f3bb4b1dcabd602c14302e3bc9d1daf82
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-04 19:52:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,7 +5,8 @@ def second_supply_for_fourth_of_july(holiday_supplies)
  end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
- holiday_hash[:winter].each do 
+ holiday_hash[:winter].each do [holiday, item] 
+ 
 end
 
 
