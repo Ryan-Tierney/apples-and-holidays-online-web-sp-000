05c7afa09d2a4607acbebc5187b4f6578b2ae6c1
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-04 20:08:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,11 +18,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall].each do |holiday, decorations|
-    :fall << holiday_name
-    decorations << supply_array
-  end 
-end
+  holiday_hash[hash][season]
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
