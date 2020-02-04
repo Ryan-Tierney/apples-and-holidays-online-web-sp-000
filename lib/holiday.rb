dc8cef39319cd2d5c86dc569d2c9f4a397347572
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-04 19:57:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -4,12 +4,11 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
  end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
+def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do [holiday, decorations]
- decorations << supply 
+ decorations << item
  end 
  end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
